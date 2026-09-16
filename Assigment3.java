dcdfb2347aaab8c90bbbb572ad2e1cc99fdaeeb1
Amina Boubakary <boubakaryamina119@gmail.com>
2026-09-16 23:19:42 +0100

    Add files via upload

---
+++ Assigment3.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class Assigment3 {
+    public static void main( String args[]){
+        String words = "hello";
+     
+        
+        System.out.println(words.endsWith("hello"));
+        System.out.println(words.intern());
+        System.out.println(words.length());
+        System.out.println(words.intern()); 
+        System.out.println(words.isEmpty());
+      }
+}
