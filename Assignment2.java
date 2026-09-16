14c1004f15aebaaf875ec2d5f442dfe4d1d8d05d
Amina Boubakary <boubakaryamina119@gmail.com>
2026-09-16 23:21:38 +0100

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class Assignment2 {
+    public static void main(String[] args) {
+       float number = (float) 54.5;
+       System.out.println("");
+        
+        }
+    }
+    
+
